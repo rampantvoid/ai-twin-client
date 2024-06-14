795948ef4907fefa621300e04dbdab13fce3679d
Priyanshu Butola <priyanshubutola@gmail.com>
2024-06-15 01:27:34 +0530

    UPDATE: Personalized Assistant

--- src/app/(app)/new/_components/step0.tsx
+++ src/app/(app)/new/_components/step0.tsx
@@ -9,7 +9,7 @@ export const Step0 = (props: StepProp) => {
   const setField = useField((store) => store.setField);
   const router = useRouter();
   return (
-    <div className="flex-1 flex flex-col items-start md:items-center lg:items-center md:px-32 lg:px-32 md:pt-24 lg:pt-24 pt-10">
+    <div className="flex-1 flex flex-col items-start md:items-center lg:items-center md:px-32 lg:px-10 md:pt-24 lg:pt-24 pt-10">
       <div className="grid grid-cols-3 items-center justify-center gap-12">
         {[
           "Educational",
@@ -34,9 +34,10 @@ export const Step0 = (props: StepProp) => {
                 setField(field);
                 router.push("/chat");
               }
+              router.push("/chat");
               // props.handleNext();
             }}
-            className="cursor-pointer bg-white rounded-[24px] flex items-center justify-start pl-10 py-5 gap-8 md:gap-2 lg:gap-8 min-h-[100px] lg:min-w-[300px] md:min-w-[200px] shadow-sm"
+            className="cursor-pointer bg-white rounded-[24px] flex items-center justify-start px-2 py-4 md:gap-2 lg:gap-8  2xl:min-w-[300px] md:min-w-[200px] shadow-sm"
             key={e}
           >
             <Image src="/file.svg" alt="file" width={40} height={40} />
